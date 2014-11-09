f6c25abc23f13966e76cfe5bf7b47ca268eafaff
loreglean <loreglean@icloud.com>
2014-11-10 00:57:35 +0300

    simple refactoring + explicit projection plane z value

--- lantern/src/pipeline.cpp
+++ lantern/src/pipeline.cpp
@@ -40,15 +40,16 @@ void pipeline::draw_mesh(
 		-camera.get_position().x, -camera.get_position().y, -camera.get_position().z, 1.0f};
 	matrix4x4 const world_to_camera_transform{camera_translation * camera_rotation};
 
+    float const projection_plane_z{1.0f};
 	float const near{camera.get_near_plane_z()};
 	float const far{camera.get_far_plane_z()};
-	float const right{std::tan(camera.get_horizontal_fov() / 2.0f) * near};
+	float const right{std::tan(camera.get_horizontal_fov() / 2.0f) * projection_plane_z};
 	float const left{-right};
-	float const top{std::tan(camera.get_vertical_fov() / 2.0f) * near};
+	float const top{std::tan(camera.get_vertical_fov() / 2.0f) * projection_plane_z};
 	float const bottom{-top};
 	matrix4x4 const camera_to_homogeneous_clip{
-		2.0f * near / (right - left), 0.0f, 0.0f, 0.0f,
-		0.0f, 2.0f * near / (top - bottom), 0.0f, 0.0f,
+		2.0f * projection_plane_z / (right - left), 0.0f, 0.0f, 0.0f,
+		0.0f, 2.0f * projection_plane_z / (top - bottom), 0.0f, 0.0f,
 		(left + right) / (left - right), (bottom + top) / (bottom - top), (far + near) / (far - near), 1.0f,
 		0.0f, 0.0f, -2.0f * near * far / (far - near), 0.0f};
 
@@ -76,6 +77,14 @@ void pipeline::draw_mesh(
 		transformed_vertices.push_back(v_transformed);
 	}
 
+    float const width{static_cast<float>(painter.get_bitmap_width())};
+    float const height{static_cast<float>(painter.get_bitmap_height())};
+    matrix4x4 const ndc_to_screen{
+            width / 2.0f, 0.0f, 0.0f, 0.0f,
+            0.0f, -height / 2.0f, 0.0f, 0.0f,
+            0.0f, 0.0f, 1.0f, 0.0f,
+            width / 2.0f, height / 2.0f, 0.0f, 1.0f};
+
 	for (vector4& v : transformed_vertices)
 	{
 		if (is_vector4_marked_as_clipped(v))
@@ -83,30 +92,14 @@ void pipeline::draw_mesh(
 			continue;
 		}
 
-		float const w_reciprocal = 1.0f / v.w;
+		float const w_reciprocal{1.0f / v.w};
 
 		v.x *= w_reciprocal;
 		v.y *= w_reciprocal;
 		v.z *= w_reciprocal;
 		v.w = 1.0f;
-	}
 
-	float const width{static_cast<float>(painter.get_bitmap_width())};
-	float const height{static_cast<float>(painter.get_bitmap_height())};
-	matrix4x4 const ndc_to_screen{
-		width / 2.0f, 0.0f, 0.0f, 0.0f,
-		0.0f, -height / 2.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, 1.0f, 0.0f,
-		width / 2.0f, height / 2.0f, 0.0f, 1.0f};
-
-	for (vector4& v : transformed_vertices)
-	{
-		if (is_vector4_marked_as_clipped(v))
-		{
-			continue;
-		}
-
-		v = v * ndc_to_screen;
+        v = v * ndc_to_screen;
 	}
 
 	for (face const& f : mesh->get_faces())
