5e938546aaa031b7edd136910f0d059a7e0c7e15
loreglean <loreglean@icloud.com>
2015-01-12 00:22:52 +0300

    zigzag traversal rasterization + tests

--- lantern/include/pipeline.h
+++ lantern/include/pipeline.h
@@ -41,8 +41,11 @@ namespace lantern
 		/** Rasterization using traversal algorithm with axis-aligned bounding box */
 		traversal_aabb,
 
-		/** Rasterization using traversal alghorithm with backtracking */
-		traversal_backtracking
+		/** Rasterization using traversal algorithm with backtracking */
+		traversal_backtracking,
+
+		/** Rasterization using traversal algorithm changing direction on every line */
+		traversal_zigzag
 	};
 
 	enum class fill_mode_option
@@ -124,6 +127,22 @@ namespace lantern
 		*/
 		template<typename TShader>
 		void rasterize_traversal_backtracking(
+			unsigned int const index0, unsigned int const index1, unsigned int const index2,
+			vector4 const& v0, vector4 const& v1, vector4 const& v2,
+			TShader& shader, texture& target_texture);
+
+		/** Rasterizes triangle using current pipeline setup using zigzag traversal algorithm
+		* @param index0 First triangle vertex index in a mesh
+		* @param index1 Second triangle vertex index in a mesh
+		* @param index2 Third triangle vertex index in a mesh
+		* @param v0 First triangle vertex
+		* @param v1 Second triangle vertex
+		* @param v2 Third triangle vertex
+		* @param shader Shader to use
+		* @param target_texture Texture to draw into
+		*/
+		template<typename TShader>
+		void rasterize_traversal_zigzag(
 			unsigned int const index0, unsigned int const index1, unsigned int const index2,
 			vector4 const& v0, vector4 const& v1, vector4 const& v2,
 			TShader& shader, texture& target_texture);
@@ -304,6 +323,13 @@ namespace lantern
 
 					case rasterization_algorithm_option::traversal_backtracking:
 						rasterize_traversal_backtracking(
+							index0, index1, index2,
+							v0, v1, v2,
+							shader, target_texture);
+						break;
+
+					case rasterization_algorithm_option::traversal_zigzag:
+						rasterize_traversal_zigzag(
 							index0, index1, index2,
 							v0, v1, v2,
 							shader, target_texture);
@@ -578,6 +604,7 @@ namespace lantern
 			}
 
 			// Moving along the scanline
+			//
 			while (true)
 			{
 				if ((!edge0_normal_pointing_right && edge0_equation_value < 0) ||
@@ -644,6 +671,241 @@ namespace lantern
 			edge2_equation_value += edge2.b;
 		}
 	}
+
+	template<typename TShader>
+	void pipeline::rasterize_traversal_zigzag(
+		unsigned int const index0, unsigned int const index1, unsigned int const index2,
+		vector4 const& v0, vector4 const& v1, vector4 const& v2,
+		TShader& shader, texture& target_texture)
+	{
+		// Construct edges equations
+		//
+
+		vector2f e0_from, e0_to;
+		vector2f e1_from, e1_to;
+		vector2f e2_from, e2_to;
+
+		// Order of points depends of wind order
+		// Because this order defines edge's normal direction
+		//
+		if (m_face_culling == face_culling_option::clockwise)
+		{
+			e0_from = vector2f{v0.x, v0.y};
+			e0_to = vector2f{v1.x, v1.y};
+
+			e1_from = vector2f{v1.x, v1.y};
+			e1_to = vector2f{v2.x, v2.y};
+
+			e2_from = vector2f{v2.x, v2.y};
+			e2_to = vector2f{v0.x, v0.y};
+		}
+		else
+		{
+			e0_from = vector2f{v1.x, v1.y};
+			e0_to = vector2f{v0.x, v0.y};
+
+			e1_from = vector2f{v2.x, v2.y};
+			e1_to = vector2f{v1.x, v1.y};
+
+			e2_from = vector2f{v0.x, v0.y};
+			e2_to = vector2f{v2.x, v2.y};
+		}
+
+		line edge0{e0_from.x, e0_from.y, e0_to.x, e0_to.y};
+		line edge1{e1_from.x, e1_from.y, e1_to.x, e1_to.y};
+		line edge2{e2_from.x, e2_from.y, e2_to.x, e2_to.y};
+
+		bool const edge0_normal_pointing_right = edge0.a > 0;
+		bool const edge1_normal_pointing_right = edge1.a > 0;
+		bool const edge2_normal_pointing_right = edge2.a > 0;
+
+		// Calculate triangle area on screen and inverse it
+		float const triangle_area_inversed = 1.0f / triangle_2d_area(v0.x, v0.y, v1.x, v1.y, v2.x, v2.y);
+
+		vector4 v0_sorted{v0};
+		vector4 v1_sorted{v1};
+		vector4 v2_sorted{v2};
+
+		// Sort vertices by y-coordinate
+		//
+
+		if (v1_sorted.y < v0_sorted.y)
+		{
+			std::swap(v0_sorted, v1_sorted);
+		}
+
+		if (v2_sorted.y < v1_sorted.y)
+		{
+			std::swap(v2_sorted, v1_sorted);
+		}
+
+		if (v1_sorted.y < v0_sorted.y)
+		{
+			std::swap(v1_sorted, v0_sorted);
+		}
+
+		// v0 is top vertex
+		// v2 is bottom vertex
+
+		vector2ui current_pixel{static_cast<unsigned int>(v0_sorted.x), static_cast<unsigned int>(v0_sorted.y)};
+		vector2f current_pixel_center{std::floor(v0_sorted.x) + 0.5f, std::floor(v0_sorted.y) + 0.5f};
+
+		float edge0_equation_value{edge0.at(current_pixel_center.x, current_pixel_center.y)};
+		float edge1_equation_value{edge1.at(current_pixel_center.x, current_pixel_center.y)};
+		float edge2_equation_value{edge2.at(current_pixel_center.x, current_pixel_center.y)};
+
+		bool start_direction_is_right{true};
+		bool is_moving_right{true};
+
+		// Index of the pixel we should move at when reach a pixel outside of a triangle from one side
+		unsigned int zigzag_x_index{current_pixel.x - 1};
+
+		while (current_pixel_center.y <= v2_sorted.y)
+		{
+			// Moving along the scanline
+			//
+			while (true)
+			{
+				// If we're moving right, we need to check pixel for being outside of a triangle from the right side
+				//
+				if (is_moving_right)
+				{
+					// If we're outside of a triangle
+					//
+					if ((!edge0_normal_pointing_right && edge0_equation_value < 0) ||
+						(!edge1_normal_pointing_right && edge1_equation_value < 0) ||
+						(!edge2_normal_pointing_right && edge2_equation_value < 0))
+					{
+						// If we moved only in side, switch direction and move to the first pixel we didn't visit yet
+						//
+						if (start_direction_is_right)
+						{
+							is_moving_right = false;
+
+							int const delta = current_pixel.x - zigzag_x_index;
+
+							current_pixel.x -= delta;
+							current_pixel_center.x -= delta * 1.0f;
+
+							edge0_equation_value -= delta * edge0.a;
+							edge1_equation_value -= delta * edge1.a;
+							edge2_equation_value -= delta * edge2.a;
+
+							continue;
+						}
+						else
+						{
+							// Otherwise scanline is over
+							break;
+						}
+					}
+				}
+				else
+				{
+					// If we're moving left, we need to check pixel for being outside of a triangle from the left side
+					//
+					if ((edge0_normal_pointing_right && edge0_equation_value < 0) ||
+						(edge1_normal_pointing_right && edge1_equation_value < 0) ||
+						(edge2_normal_pointing_right && edge2_equation_value < 0))
+					{
+						// If we moved only in side, switch direction and move to the first pixel we didn't visit yet
+						//
+						if (!start_direction_is_right)
+						{
+							is_moving_right = true;
+
+							int const delta = zigzag_x_index - current_pixel.x;
+
+							current_pixel.x += delta;
+							current_pixel_center.x += delta * 1.0f;
+
+							edge0_equation_value += delta * edge0.a;
+							edge1_equation_value += delta * edge1.a;
+							edge2_equation_value += delta * edge2.a;
+
+							continue;
+						}
+						else
+						{
+							// Otherwise scanline is over
+							break;
+						}
+					}
+				}
+
+				if (is_point_on_positive_halfplane_top_left(edge0_equation_value, edge0.a, edge0.b) &&
+					is_point_on_positive_halfplane_top_left(edge1_equation_value, edge1.a, edge1.b) &&
+					is_point_on_positive_halfplane_top_left(edge2_equation_value, edge2.a, edge2.b))
+				{
+					float const area01{triangle_2d_area(v0.x, v0.y, v1.x, v1.y, current_pixel_center.x, current_pixel_center.y)};
+					float const area12{triangle_2d_area(v1.x, v1.y, v2.x, v2.y, current_pixel_center.x, current_pixel_center.y)};
+
+					// Calculate barycentric coordinates
+					//
+					float const b2{area01 * triangle_area_inversed};
+					float const b0{area12 * triangle_area_inversed};
+					float const b1{1.0f - b0 - b2};
+
+					// Process different attributes
+					//
+
+					set_bind_points_values_from_barycentric<color>(
+						m_binded_color_attributes,
+						index0, index1, index2,
+						b0, b1, b2);
+
+					set_bind_points_values_from_barycentric<float>(
+						m_binded_float_attributes,
+						index0, index1, index2,
+						b0, b1, b2);
+
+					set_bind_points_values_from_barycentric<vector2f>(
+						m_binded_vector2f_attributes,
+						index0, index1, index2,
+						b0, b1, b2);
+
+					set_bind_points_values_from_barycentric<vector3>(
+						m_binded_vector3_attributes,
+						index0, index1, index2,
+						b0, b1, b2);
+
+					// Pass pixel to shader
+					//
+					target_texture.set_pixel_color(current_pixel, shader.process_pixel(current_pixel));
+				}
+
+				if (is_moving_right)
+				{
+					current_pixel.x += 1;
+					current_pixel_center.x += 1.0f;
+
+					edge0_equation_value += edge0.a;
+					edge1_equation_value += edge1.a;
+					edge2_equation_value += edge2.a;
+				}
+				else
+				{
+					current_pixel.x -= 1;
+					current_pixel_center.x -= 1.0f;
+
+					edge0_equation_value -= edge0.a;
+					edge1_equation_value -= edge1.a;
+					edge2_equation_value -= edge2.a;
+				}
+			}
+
+			current_pixel.y += 1;
+			current_pixel_center.y += 1.0f;
+
+			edge0_equation_value += edge0.b;
+			edge1_equation_value += edge1.b;
+			edge2_equation_value += edge2.b;
+
+			start_direction_is_right = true;
+			is_moving_right = true;
+			zigzag_x_index = current_pixel.x;
+		}
+	};
 
 	inline bool pipeline::is_point_on_positive_halfplane_top_left(
 		float const edge_equation_value, float const edge_equation_a, float const edge_equation_b) const
--- lantern/src/pipeline.cpp
+++ lantern/src/pipeline.cpp
@@ -4,7 +4,7 @@ using namespace lantern;
 
 pipeline::pipeline()
 {
-	set_rasterization_algorithm(rasterization_algorithm_option::traversal_backtracking);
+	set_rasterization_algorithm(rasterization_algorithm_option::traversal_zigzag);
 	set_face_culling(face_culling_option::counter_clockwise);
 	set_fill_mode(fill_mode_option::solid);
 }
