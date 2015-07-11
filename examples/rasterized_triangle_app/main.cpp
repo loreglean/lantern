f1a267238163e179d5e4ffb40b4ecd5438aec0e5
loreglean <loreglean@gmail.com>
2015-07-11 16:19:05 +0300

    rasterized_triangle_app: switching between colored and textured modes

--- examples/rasterized_triangle_app/main.cpp
+++ examples/rasterized_triangle_app/main.cpp
@@ -2,8 +2,15 @@
 #include "obj_import.h"
 #include "camera.h"
 #include "color_shader.h"
+#include "texture_shader.h"
 
 using namespace lantern;
+
+enum class shader_option
+{
+	color,
+	texture
+};
 
 /** Draws simple triangle with interpolated color attribute */
 class rasterized_color_triangle_app : public app
@@ -24,7 +31,12 @@ private:
 	mesh m_triangle_mesh;
 
 	camera m_camera;
+	
 	color_shader m_color_shader;
+	texture_shader m_texture_shader;
+	shader_option m_shader_option;
+
+	texture m_texture;
 };
 
 rasterized_color_triangle_app::rasterized_color_triangle_app(unsigned int const width, unsigned int const height)
@@ -39,23 +51,45 @@ rasterized_color_triangle_app::rasterized_color_triangle_app(unsigned int const 
 		  static_cast<float>(M_PI) / 2.0f,
 		  static_cast<float>(height) / static_cast<float>(width),
 		  0.01f,
-		  20.0f}
+		  20.0f},
+	  m_shader_option{shader_option::color},
+	  m_texture{texture::load_from_file("resources/chess.png")}
 {
 	// Update model-view-projection matrix for the first time
 	update_shader_mvp();
 
+	std::vector<unsigned int> const indices{0, 1, 2};
+
 	// Add color attribute to triangle mesh
 	//
 	std::vector<color> const colors{color::GREEN, color::RED, color::BLUE};
-	std::vector<unsigned int> const indices{0, 1, 2};
 	mesh_attribute_info<color> const color_info{VERTEX_COLOR_ATTR_ID, colors, indices, attribute_interpolation_option::linear};
 	m_triangle_mesh.get_color_attributes().push_back(color_info);
+
+	// Add uv attribute to triangle mesh
+	//
+	std::vector<vector2f> uvs{vector2f{0.5f, 0.0f}, vector2f{0.0f, 1.0f}, vector2f{1.0f, 1.0f}};
+	mesh_attribute_info<vector2f> uv_info{TEXCOORD_ATTR_ID, uvs, indices, attribute_interpolation_option::perspective_correct};
+	m_triangle_mesh.get_vector2f_attributes().push_back(uv_info);
+
+	// Setup texture shader
+	//
+	m_texture_shader.set_texture(&m_texture);
 };
 
 void rasterized_color_triangle_app::frame(float const delta_since_last_frame)
 {
 	// Draw the triangle
-	get_pipeline().draw(m_triangle_mesh, m_color_shader, get_target_texture());
+	//
+
+	if (m_shader_option == shader_option::color)
+	{
+		get_pipeline().draw(m_triangle_mesh, m_color_shader, get_target_texture());
+	}
+	else if (m_shader_option == shader_option::texture)
+	{
+		get_pipeline().draw(m_triangle_mesh, m_texture_shader, get_target_texture());
+	}
 }
 
 void rasterized_color_triangle_app::on_key_down(SDL_Keysym const key)
@@ -97,6 +131,14 @@ void rasterized_color_triangle_app::on_key_down(SDL_Keysym const key)
 	{
 		m_camera.yaw(rotation_speed);
 	}
+	else if (key.sym == SDLK_1)
+	{
+		m_shader_option = shader_option::color;
+	}
+	else if (key.sym == SDLK_2)
+	{
+		m_shader_option = shader_option::texture;
+	}
 
 	// Update model-view-projection according to camera changes
 	update_shader_mvp();
@@ -134,7 +176,14 @@ void rasterized_color_triangle_app::update_shader_mvp()
 	matrix4x4 const local_to_clip_transform{
 		local_to_world_transform * world_to_camera_transform * camera_to_clip_transform};
 
-	m_color_shader.set_mvp_matrix(local_to_clip_transform);
+	if (m_shader_option == shader_option::color)
+	{
+		m_color_shader.set_mvp_matrix(local_to_clip_transform);
+	}
+	else
+	{
+		m_texture_shader.set_mvp_matrix(local_to_clip_transform);
+	}
 }
 
 int main(int argc, char* argv[])
